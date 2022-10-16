a9b3b3df68a852d50db0bbf3d5119faecfd98ca3
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2022-10-16 11:00:04 -0500

    empecé el diseño de la función para visualizar funciones

---
+++ clases/3_funciones/proporcionalidad/proporcionalidad_inversa/proporcionalidad.py
@@ -0,0 +1,15 @@
+# construcción de funciones para visualizar modelos
+
+def visualizar_funcion(dimensiones_figura = (8,8)):
+    import numpy as np 
+    from matplotlib import pyplot as plt 
+    plt.figure(figsize=dimensiones_figura)
+
+
+
+
+
+if __name__=='__main__':
+    pass 
+
+
