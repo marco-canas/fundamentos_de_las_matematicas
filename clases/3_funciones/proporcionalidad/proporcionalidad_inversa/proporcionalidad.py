1a67aab4b9c512c5eddfb79f19f04452e0920069
Marco Julio Cañas Campillo <66282835+marco-canas@users.noreply.github.com>
2022-10-20 10:43:26 -0500

    empezamos la modelación de l flujo de  sangre

--- clases/3_funciones/proporcionalidad/proporcionalidad_inversa/proporcionalidad.py
+++ clases/3_funciones/proporcionalidad/proporcionalidad_inversa/proporcionalidad.py
@@ -1,9 +1,11 @@
 # construcción de funciones para visualizar modelos
 
-def visualizar_funcion(dimensiones_figura = (8,8)):
+def visualizar_funcion(dimensiones_figura = (8,8), titulo = 'grafico función'):
     import numpy as np 
     from matplotlib import pyplot as plt 
     plt.figure(figsize=dimensiones_figura)
+    plt.title(titulo)
+    plt.show() 
 
 
 
